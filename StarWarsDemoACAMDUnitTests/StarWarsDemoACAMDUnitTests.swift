de70171eab789444409db148a0c715fc0bd32a3f
aaesalamanca <aaesalamanca@gmail.com>
2024-10-29 23:19:31 +0100

    test: Card should have all skills capitalized

--- StarWarsDemoACAMDUnitTests/StarWarsDemoACAMDUnitTests.swift
+++ StarWarsDemoACAMDUnitTests/StarWarsDemoACAMDUnitTests.swift
@@ -38,4 +38,32 @@ struct StarWarsDemoACAMDUnitTests {
         // Assert.
         #expect(expected == actual)
     }
+
+    @Test func cardShouldHaveAllSkillsCapitalized() async throws {
+        // Arrange.
+        let sut = CardDTO(
+            nombre: "Luke Skywalker",
+            raza: "Humano",
+            descripcion:
+                "Héroe de la Alianza Rebelde y Caballero Jedi. Hijo de Anakin Skywalker y Padmé Amidala.",
+            planetaOrigen: "Tatooine",
+            epoca: "Era del Imperio Galáctico",
+            afiliacion: "Alianza Rebelde, Nueva República",
+            habilidades: "Uso de la Fuerza, combate con sable de luz, pilotaje",
+            armas: "Sable de luz",
+            id: 6629,
+            imagen: "lukeskywalker"
+        )
+        let expected = [
+            "Uso De La Fuerza", "Combate Con Sable De Luz", "Pilotaje",
+        ]
+
+        // Act.
+        let actual = sut.toCard.habilidades
+
+        // Assert.
+        for i in 0..<expected.count {
+            #expect(expected[i] == actual[i])
+        }
+    }
 }
