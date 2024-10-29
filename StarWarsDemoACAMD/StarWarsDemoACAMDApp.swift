1a8d4efcb1b90d13d2a1dd4556a8aa7f82357e70
aaesalamanca <aaesalamanca@gmail.com>
2024-10-29 12:38:51 +0100

    Initial Commit

---
+++ StarWarsDemoACAMD/StarWarsDemoACAMDApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsDemoACAMDApp.swift
+//  StarWarsDemoACAMD
+//
+//  Created by Alejandro Antonio Estornell Salamanca on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsDemoACAMDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
