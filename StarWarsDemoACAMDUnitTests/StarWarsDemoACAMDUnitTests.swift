3ff80ddb73752bca66f7d49da997d36d04032d0b
aaesalamanca <aaesalamanca@gmail.com>
2024-10-29 15:52:35 +0100

    test: Add unit testing target

---
+++ StarWarsDemoACAMDUnitTests/StarWarsDemoACAMDUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  StarWarsDemoACAMDUnitTests.swift
+//  StarWarsDemoACAMDUnitTests
+//
+//  Created by Alejandro Antonio Estornell Salamanca on 29/10/24.
+//
+
+import Testing
+
+struct StarWarsDemoACAMDUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
